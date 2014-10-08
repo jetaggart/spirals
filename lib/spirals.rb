b1885bb66ad609fcfb96da26837a86b57e5f3e65
Jeff Taggart <jeff@jetaggart.com>
2014-10-08 10:10:37 -0600

    done

--- lib/spirals.rb
+++ lib/spirals.rb
@@ -1,12 +1,47 @@
 class Spirals
   attr_reader :starting, :width
+  attr_accessor :result, :starting
 
   def initialize(starting, width)
-    @starting = starting
-    @width = width
+    @starting = starting - 1
+    @width    = width
+    @result   = width.times.map { [] }
   end
 
   def generate
-    width.times.map { starting }
+    position = {:x => 0, :y => 0}
+    direction = {:x => :pos, :y => :pos}
+    current_dir = :y
+    numbers_placed = 0
+
+    while (numbers_placed < (width * width)) do
+      if in_bounds(position[current_dir]) && result[position[:x]][position[:y]].nil?
+        result[position[:x]][position[:y]] = next_number
+        next_position(current_dir, direction, position)
+        numbers_placed += 1
+      else
+        direction[current_dir] = direction[current_dir] == :pos ? :neg : :pos
+        next_position(current_dir, direction, position)
+
+        current_dir = current_dir == :y ? :x : :y
+        next_position(current_dir, direction, position)
+      end
+    end
+
+    result
+  end
+
+  def next_position(current_dir, direction, position)
+    direction[current_dir] == :pos ? position[current_dir] += 1 : position[current_dir] -= 1
+  end
+
+  private
+
+  def next_number
+    self.starting += 1
+  end
+
+  def in_bounds(coordinate)
+    coordinate < width
   end
 end
--- spec/sprials_spec.rb
+++ spec/sprials_spec.rb
@@ -12,12 +12,27 @@ describe Spirals do
     it "returns only the number for width of 1" do
       pattern = Spirals.new(0, 1).generate
 
-      expect(pattern).to match_array([0])
+      expect(pattern).to match_array([[0]])
 
       pattern = Spirals.new(1, 1).generate
 
-      expect(pattern).to match_array([1])
+      expect(pattern).to match_array([[1]])
     end
 
+    it "returns a 2x2 with width 2" do
+      pattern = Spirals.new(1, 2).generate
+
+      expect(pattern).to match_array([[1, 2],
+                                      [4, 3]])
+    end
+
+    it "returns a 4x4 with width 4" do
+      pattern = Spirals.new(1, 4).generate
+
+      expect(pattern).to match_array([[1,  2,  3,  4],
+                                      [12, 13, 14, 5],
+                                      [11, 16, 15, 6],
+                                      [10, 9,  8,  7]])
+    end
   end
 end
